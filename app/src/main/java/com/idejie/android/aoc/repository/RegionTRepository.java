06b5d7b8b1e41a48edafa866f5f330639087c358
slf <971143573@qq.com>
2016-08-31 21:30:41 +0800

    提交保存进度

--- app/src/main/java/com/idejie/android/aoc/repository/RegionTRepository.java
+++ app/src/main/java/com/idejie/android/aoc/repository/RegionTRepository.java
@@ -1,17 +1,17 @@
 package com.idejie.android.aoc.repository;
 
+import com.idejie.android.aoc.model.Region;
 import com.idejie.android.aoc.model.RegionModel;
-import com.idejie.android.aoc.model.SortModel;
 import com.strongloop.android.loopback.ModelRepository;
 
 /**
  * Created by slf on 16/8/30.
  */
 
-public class RegionRepository extends ModelRepository<RegionModel> {
+public class RegionTRepository extends ModelRepository<Region> {
 
-    public RegionRepository() {
-        super("region", RegionModel.class);
+    public RegionTRepository() {
+        super("region", Region.class);
     }
 
 
--- app/src/main/java/com/idejie/android/aoc/tools/MyHashMap.java
+++
@@ -1,8 +0,0 @@
-package com.idejie.android.aoc.tools;
-
-import java.util.HashMap;
-
-/**
- * Created by slf on 16/8/31.
- */
-
