6aaffd23647766ac1c86bbadfcf9a3dff506dec2
idejie <i@idejie.com>
2016-09-02 13:45:34 +0800

    保存一下进度

--- app/src/main/java/com/idejie/android/aoc/fragment/tab/SecondLayerFragment.java
+++ app/src/main/java/com/idejie/android/aoc/fragment/tab/SecondLayerFragment.java
@@ -18,16 +18,19 @@ import com.idejie.android.library.view.indicator.IndicatorViewPager;
 import com.strongloop.android.loopback.RestAdapter;
 import com.strongloop.android.loopback.callbacks.ListCallback;
 import com.strongloop.android.loopback.callbacks.ObjectCallback;
+import com.strongloop.android.remoting.adapters.Adapter;
 import com.youth.banner.Banner;
 import com.youth.banner.BannerConfig;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 /**
  * Created by shandongdaxue on 16/8/8.
  */
-public class SecondLayerFragment extends LazyFragment {
+public class SecondLayerFragment extends LazyFragment implements SwipeRefreshLayout.OnRefreshListener {
     public static final String INTENT_STRING_TABNAME = "intent_String_tabName";
     public static final String INTENT_INT_POSITION = "intent_int_position";
     public static final String URL_API = "http://192.168.1.114:3001/api/";
@@ -71,8 +74,13 @@ public class SecondLayerFragment extends LazyFragment {
             @Override
             public void OnBannerClick(View view, int position) {
                 Toast.makeText(getApplicationContext(),"你点击了："+position,Toast.LENGTH_LONG).show();
+                queryYaowen(true);
             }
         });
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
+        swipeRefreshLayout.setOnRefreshListener(this);
+        queryYaowen(true);
     }
 
     private void startZhangwang() {
@@ -107,7 +115,7 @@ public class SecondLayerFragment extends LazyFragment {
                 Toast.makeText(getApplicationContext(),"你点击了："+position,Toast.LENGTH_LONG).show();
             }
         });
-        getData();
+//        getData();
     }
 
     private void startYaowen() {
@@ -123,6 +131,56 @@ public class SecondLayerFragment extends LazyFragment {
             @Override
             public void OnBannerClick(View view, int position) {
                 Toast.makeText(getApplicationContext(),"你点击了："+position,Toast.LENGTH_LONG).show();
+            }
+        });
+        queryYaowen(true);
+    }
+
+    private void queryYaowen(boolean b) {
+        if (!CommonUtil.checkNetState(getActivity())){
+            swipeRefreshLayout.setRefreshing(false);
+            Toast.makeText(getContext(),"请检查网络连接",Toast.LENGTH_LONG).show();
+        }
+        RestAdapter adapter = new RestAdapter(getApplicationContext(), "http://192.168.1.114:3001/api");
+        adapter.setAccessToken("4miVFTq2Yt3nDPPrTLLvJGSQNKH5k0x78fNyHENbwyICjii206NqmjL5ByChP6dO");
+        NewsRepository newRepo=adapter.createRepository(NewsRepository.class);
+        Map<String,Object> params =new HashMap<String,Object>();
+//        Map<String,Object>filter=new HashMap<String,Object>();
+//        filter.put("include","new");
+//            pramas.put("filter",filter);
+        newRepo.invokeStaticMethod("count", null, new Adapter.Callback() {
+            @Override
+            public void onSuccess(String response) {
+                Toast.makeText(getContext(),response,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                Toast.makeText(getContext(),t.toString(),Toast.LENGTH_LONG).show();
+            }
+        });
+        newRepo.findById(0,new ObjectCallback<NewsModel>(){
+
+            @Override
+            public void onSuccess(NewsModel object) {
+
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+        });
+        newRepo.findAll(new ListCallback<NewsModel>() {
+            @Override
+            public void onSuccess(List<NewsModel> objects) {
+                Toast.makeText(getContext(),"标题:"+objects.get(0).getTitle()+"\n摘要:"+objects.get(0).getSummary()+"\n内容:"+objects.get(0).getContent()+"\n图片ID:"+objects.get(0).getImageId()+"\n标签id:"+objects.get(0).getTagId(),Toast.LENGTH_LONG).show();
+                Log.d("aoc=======","==="+objects.get(0).toString());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                Toast.makeText(getContext(),"失败了",Toast.LENGTH_LONG).show();
             }
         });
     }
@@ -168,35 +226,8 @@ public class SecondLayerFragment extends LazyFragment {
     }
 
 
-    public void getData() {
-        RestAdapter adapter = new RestAdapter(getApplicationContext(),URL_API);
-        Toast.makeText(getApplicationContext(),""+adapter,Toast.LENGTH_LONG).show();
-        adapter.setAccessToken(TOKEN);
-        NewsRepository newsRepository =adapter.createRepository(NewsRepository.class);
-        newsRepository.findById(0, new ObjectCallback<NewsModel>() {
-            @Override
-            public void onSuccess(NewsModel object) {
-                Log.d("news test","============"+object.toString());
-            }
-
-            @Override
-            public void onError(Throwable t) {
-                Log.d("news test","============"+t.toString());
-            }
-        });
-        newsRepository.findAll(new ListCallback<NewsModel>() {
-            @Override
-            public void onSuccess(List<NewsModel> objects) {
-                Log.d("news test all","============"+objects.toString());
-
-            }
-
-            @Override
-            public void onError(Throwable t) {
-                Log.d("news test all","============"+t.toString());
-            }
-        });
+    @Override
+    public void onRefresh() {
+        queryYaowen(true);
     }
-
-
 }
